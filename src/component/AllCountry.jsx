817c203ef653947ce7fdacda2a81fc78199cbf8a
Suranjit Mondal <suranjitm07@gmail.com>
2023-10-06 00:33:28 +0600

    hello

--- src/component/AllCountry.jsx
+++ src/component/AllCountry.jsx
@@ -5,17 +5,49 @@ import Country from "./country";
 
 const AllCountry = () => {
     const [countrys, setCuntrys] = useState([]);
-    useEffect(() =>{
-        fetch ('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCuntrys(data))
-    },[])
+    const [countryName, setCountryName] = useState([]);
+
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCuntrys(data))
+    }, [])
+    const CountryNameFuntion = countr => {
+        const showCuntry = [...countryName, countr];
+        setCountryName(showCuntry);
+    }
     return (
-        <div>
-            <h3>Countrys :{countrys.length}</h3>
-            {
-              countrys.map(con => <Country key={con.cca3} count={con}></Country>)  
-            }
+
+        <div >
+            <h3 className="text-center text-4xl p-4">Countrys :{countrys.length}</h3>
+            <h3 className="text-center text-xl p-4">Visited Countrys :{countryName.length}</h3>
+            <div>
+                {
+                    countryName.map(country =>
+                      
+                            <div className="  bg-base-100 shadow-xl ">
+                                <figure><img className="w-[100px] bg-base-100 shadow-xl" src={country.flags.png} alt="" /></figure>
+                                
+                                <h3 className="text-[10px]">{country.name?.common}</h3>
+                               
+
+                            </div>
+                        )
+                }
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    countrys.map(con => <Country
+                        key={con.cca3}
+                        sentCuntry={CountryNameFuntion}
+                        count={con}>
+
+                    </Country>)
+                }
+
+            </div>
 
         </div>
     );
--- src/component/country.jsx
+++ src/component/country.jsx
@@ -1,15 +1,33 @@
-const Country = ({ count }) => {
-    const { name, flags } = count;
+import { useState } from "react";
+
+const Country = ({ count, sentCuntry }) => {
+    const { name, flags,population,area, cca3,} = count;
+    const [visite, setVisite] = useState(false);
+
+    
+    const VisitedHandeler = ()=>{
+        setVisite(!visite);
+        
+    }
+    // console.log(sentCuntry);
     return (
         <div  >
-            <section className="grid grid-cols-4">
-                <div className="card w-96 bg-base-100 shadow-xl m-4 ">
+            <section className="">
+                <div className="card flex bg-base-100 shadow-xl m-4 ">
                     <figure><img src={flags.png} alt="" /></figure>
                     <div className="card-body">
-                        <h3>hello:</h3>
                         <h3>Name: {name?.common}</h3>
-
+                        <p>Population :{population}</p>
+                        <p>Area:{area}</p>
+                        <p>Code : {cca3}</p>
+                        
                     </div>
+                    <div className="text-center ">
+                    {visite && 'I Have Visited This Cuntory.'}
+                    </div>
+                    <button className="bg-red-200" onClick={()=>sentCuntry(count)}>Mark-UP</button>
+                    <button onClick={VisitedHandeler}>{visite?'Visited':'Going'}</button>
+                   
                 </div>
             </section>
         </div>
